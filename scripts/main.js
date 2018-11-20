8fb71f9ec4f570e57517c80f9e8fe0b3f99f2e30
Arthur Provost <arthurprovost@dy087081.etu.univ-rennes1.fr>
2018-11-20 16:41:53 +0100

    add header video

---
+++ scripts/main.js
@@ -0,0 +1,37 @@
+$(function() {
+    /**
+    * Smooth scrolling to page anchor on click
+    **/
+	 $('a[href^="#"]').click(function(){
+		var the_id = $(this).attr("href");
+		if (the_id === '#') {
+			return;
+		}
+		$('html, body').animate({
+			scrollTop:$(the_id).offset().top
+		}, 'slow');
+		return false;
+	});
+
+
+	// Vars.
+		var	$window = $(window),
+			$body = $('body'),
+			$wrapper = $('#wrapper');
+
+	// Breakpoints.
+		skel.breakpoints({
+			xlarge:	'(max-width: 1680px)',
+			large:	'(max-width: 1280px)',
+			medium:	'(max-width: 980px)',
+			small:	'(max-width: 736px)',
+			xsmall:	'(max-width: 480px)'
+		});
+
+	// Disable animations/transitions until everything's loaded.
+		$body.addClass('is-loading');
+
+		$window.on('load', function() {
+			$body.removeClass('is-loading');
+		});
+});
