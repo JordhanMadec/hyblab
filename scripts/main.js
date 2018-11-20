defd9e58c07b101f9c291dfde283ca568a1a89b2
Arthur Provost <arthurprovost@dy087081.etu.univ-rennes1.fr>
2018-11-20 16:57:25 +0100

    fix

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@ $(function() {
     /**
     * Smooth scrolling to page anchor on click
     **/
-/*	 $('a[href^="#"]').click(function(){
+	 $('a[href^="#"]').click(function(){
 		var the_id = $(this).attr("href");
 		if (the_id === '#') {
 			return;
@@ -12,26 +12,26 @@ $(function() {
 		}, 'slow');
 		return false;
 	});
-*/
+
 
 	// Vars.
-		var	$window = $(window),
-			$body = $('body'),
-			$wrapper = $('#wrapper');
+	var	$window = $(window),
+		$body = $('body'),
+		$wrapper = $('#wrapper');
 
 	// Breakpoints.
-		skel.breakpoints({
-			xlarge:	'(max-width: 1680px)',
-			large:	'(max-width: 1280px)',
-			medium:	'(max-width: 980px)',
-			small:	'(max-width: 736px)',
-			xsmall:	'(max-width: 480px)'
-		});
+	skel.breakpoints({
+		xlarge:	'(max-width: 1680px)',
+		large:	'(max-width: 1280px)',
+		medium:	'(max-width: 980px)',
+		small:	'(max-width: 736px)',
+		xsmall:	'(max-width: 480px)'
+	});
 
 	// Disable animations/transitions until everything's loaded.
-		$body.addClass('is-loading');
+	$body.addClass('is-loading');
 
-		$window.on('load', function() {
-			$body.removeClass('is-loading');
-		});
+	$window.on('load', function() {
+		$body.removeClass('is-loading');
+	});
 });
