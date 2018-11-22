f3ff5f473027b31c27366c5222ce8f230b51efc2
Jordhan <jordhan.madec@gmail.com>
2018-11-22 21:06:18 +0100

    add data file

--- scripts/map.js
+++ scripts/map.js
@@ -22,11 +22,11 @@ map.on('load', function() {
     map.setPaintProperty('water', 'fill-color', '#000');
     // Add a new source from our GeoJSON data and set the
     // 'cluster' option to true. GL-JS will add the point_count property to your source data.
-    map.addSource("earthquakes", {
+    map.addSource("patrimony", {
         type: "geojson",
         // Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
         // from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
-        data: "https://www.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson",
+        data: "jordhan.cjot0q28l0pbs2wmu4wz7g07n-9te1c",
         cluster: true,
         clusterMaxZoom: 14, // Max zoom to cluster points on
         clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
@@ -35,7 +35,7 @@ map.on('load', function() {
     map.addLayer({
         id: "clusters",
         type: "circle",
-        source: "earthquakes",
+        source: "patrimony",
         filter: ["has", "point_count"],
         paint: {
             // Use step expressions (https://www.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
@@ -63,11 +63,11 @@ map.on('load', function() {
             ]
         }
     });
-
+/*
     map.addLayer({
         id: "cluster-count",
         type: "symbol",
-        source: "earthquakes",
+        source: "patrimony",
         filter: ["has", "point_count"],
         layout: {
             "text-field": "{point_count_abbreviated}",
@@ -79,7 +79,7 @@ map.on('load', function() {
     map.addLayer({
         id: "unclustered-point",
         type: "circle",
-        source: "earthquakes",
+        source: "patrimony",
         filter: ["!", ["has", "point_count"]],
         paint: {
             "circle-color": "#11b4da",
@@ -87,7 +87,7 @@ map.on('load', function() {
             "circle-stroke-width": 1,
             "circle-stroke-color": "#fff"
         }
-    });
+    });*/
 
     // inspect a cluster on click
     map.on('click', 'clusters', function (e) {
