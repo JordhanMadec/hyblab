01a6c558b4798ca81e6eacba608ed607abafc078
Mathieu THE <matieuthe@gmail.com>
2018-12-03 14:52:10 +0100

    premiere version estimation

--- scripts/tweetPart.js
+++ scripts/tweetPart.js
@@ -1,25 +1,6 @@
 function displayInfo(nb){
     document.getElementById("boites").style.display = "none";
     document.getElementById("bien"+nb).style.display = "block";
-/*
-    var toDisplay = false;
-    for(var i = 1; i <= 3; i++){
-        if( i != nb && document.getElementById("bien" + i).style.display == "none"){
-            toDisplay = true;
-        }
-    }
-    if(!toDisplay){
-        for(var i = 1; i <= 3; i++){
-            if( i != nb){
-                document.getElementById("bien" + i).style.display = "none";
-            }
-        }
-        document.getElementById("infoBien" + nb).style.display = "block";
-    }else{
-        document.getElementById("infoBien" + nb).style.display = "none";
-        for(var i = 1; i <= 3; i++)
-            document.getElementById("bien" + i).style.display = "block";
-    }*/
 }
 
 function openTwitter(nb){
@@ -29,3 +10,21 @@ function openTwitter(nb){
     var lien = "https://twitter.com/intent/tweet?button_hashtag=" + HashTag + "&ref_src=twsrc%5Etfw&text=" + text;
     window.open(lien);
 }
+
+$("#prixUtilisateur1").on('keypress', function (e) {
+    if(e.which === 13) { // Press enter key
+        openTwitter(1);
+    }
+});
+
+$("#prixUtilisateur2").on('keypress', function (e) {
+    if(e.which === 13) { // Press enter key
+        openTwitter(2);
+    }
+});
+
+$("#prixUtilisateur3").on('keypress', function (e) {
+    if(e.which === 13) { // Press enter key
+        openTwitter(3);
+    }
+});
