5974462bf40edb48ca49246205a6bd2729bf7dc2
Mathieu THE <matieuthe@gmail.com>
2018-12-04 10:17:11 +0100

    ajout var

--- public/scripts/statLocal.js
+++ public/scripts/statLocal.js
@@ -1,8 +1,61 @@
 function checkStat(params) {
 
+    //Type de biens
+    let espaceNaturels = 0;
+    let logements = 0;
+    let bureaux = 0;
+    let espaceAmenage = 0;
+    let batTechnique = 0;
+    let voirie = 0;
+    let parcelles = 0;
+    let sport = 0;
+    let sanitaire = 0;
+    let commerce = 0;
+    let culturel = 0;
+    let agricole = 0;
+    let memorial = 0;
+    let culte = 0;
 
-    
-    for(var i=0;i<dataCsV.length;i++)
+    //Type de ventes
+    let greagre = 0;
+    let droitDePro = 0;
+    let appelDoffre = 0;
+    let adjujication = 0;
+    let autresDroit = 0;
+    let echange = 0;
+    let recoursAgence = 0;
+    let VNI = 0;
+
+    //Occupant
+    let ecologieM = 0;
+    let defenseM = 0;
+    let agricultureM = 0;
+    let comptePubM = 0;
+    let interieurM = 0;
+    let educationM = 0;
+    let justiceM = 0;
+    let cultureM = 0;
+    let economieM = 0;
+    let travailM = 0;
+    let foretM = 0;
+    let affaireEtrangeresM = 0;
+    let santeM = 0;
+    let serviceMinistreM = 0;
+
+    //Acquereurs
+    let particuliers = 0;
+    let collectivites = 0;
+    let entreprise = 0;
+    let etabPubTerri = 0;
+    let asso = 0;
+    let persoEtr = 0;
+
+    //Duree de ventes
+    let moinsUnAn = 0;
+    let entreUnetDe = 0;
+    let plusDeDeux = 0;
+
+    for(var i=0;i<dataCSV.length;i++)
     {
         let latitude = dataCsV[i]["latitude"];
         let longitude = dataCsV[i]["longitude"];
